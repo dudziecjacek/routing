a31852b11b4b1d7e428c2e37bf4f49ce30b12bfc
jdudziec <dudziecjacek@gmail.com>
2021-08-04 20:47:19 +0200

    shallow testing

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { HeroComponent } from './hero.component';
+
+describe('HeroComponent', () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent]
+    });
+    fixture = TestBed.createComponent(HeroComponent);
+  });
+
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+
+    expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+  });
+
+  it('should render the hero name in an anchor tag', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('a')).nativeElement.textContent).toContain('SuperDude');
+
+    // expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+  });
+});
