6df489721a7437a3100812da9c52d6692a7abd9d
jdudziec <dudziecjacek@gmail.com>
2021-08-05 10:28:54 +0200

    added hero.service

---
+++ src/app/hero.service.spec.ts
@@ -0,0 +1,34 @@
+import { MessageService } from './message.service';
+import { TestBed, inject } from '@angular/core/testing';
+import { HeroService } from './hero.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+describe('HeroService', () => {
+  let mockMessageService;
+  let httpTestingController: HttpTestingController;
+  let service;
+
+  beforeEach(() => {
+    mockMessageService = jasmine.createSpyObj(['add', 'clear']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HeroService,
+        { provide: MessageService, useValue: mockMessageService }
+      ]
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(HeroService);
+  });
+
+  describe('getHero', () => {
+    it('should call get with the correct URL', () => {
+      service.getHero(4).subscribe();
+
+      const req = httpTestingController.expectOne('api/heroes/4');
+      req.flush({ id: 4, name: 'SuperDude', strength: 100 });
+      httpTestingController.verify();
+    });
+  });
+});
