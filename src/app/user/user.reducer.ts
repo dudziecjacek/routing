8dc58ea95c5264989b4e783f5817a705eb98df3c
jdudziec <dudziecjacek@gmail.com>
2021-09-28 09:06:30 +0200

    added selectors

--- src/app/user/user.reducer.ts
+++ src/app/user/user.reducer.ts
@@ -1,7 +1,35 @@
-import { createAction, createReducer, on } from '@ngrx/store';
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from '@ngrx/store';
+import * as AppState from '../state/app.state';
+import { User } from './user';
 
-export const userReducer = createReducer(
-  { maskUserName: true },
+export interface State extends AppState.State {
+  users: UserState;
+}
+
+export interface UserState {
+  maskUserName: boolean;
+  currentUser: User;
+}
+
+const initialState: UserState = {
+  maskUserName: true,
+  currentUser: null
+};
+
+const getUserFeatureState = createFeatureSelector<UserState>('users');
+
+export const getMaskUserName = createSelector(
+  getUserFeatureState,
+  state => state.maskUserName
+);
+
+export const getCurrentUser = createSelector(
+  getUserFeatureState,
+  state => state.currentUser
+);
+
+export const userReducer = createReducer<UserState>(
+  initialState,
   on(createAction('[Product] Toggle username masking'), state => {
     console.log(state);
     return { ...state, maskUserName: !state.maskUserName };
